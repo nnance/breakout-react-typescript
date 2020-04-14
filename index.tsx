c7f89b46ce8e98d4daae882f953dbfdcc7f5d3b7
Nick Nance <nance.nick@gmail.com>
2020-04-13 21:02:55 -0700

    initial board

--- index.tsx
+++ index.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export const Main: React.FC = () => <div>Hello World!</div>;
+const startGame = (canvas: HTMLCanvasElement) => {
+  const wallSize = 12;
+  const context = canvas.getContext("2d");
 
-ReactDOM.render(<Main/>, document.querySelector("#root"));
+  if (context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw walls
+    context.fillStyle = "lightgrey";
+    context.fillRect(0, 0, canvas.width, wallSize);
+    context.fillRect(0, 0, wallSize, canvas.height);
+    context.fillRect(canvas.width - wallSize, 0, wallSize, canvas.height);
+  }
+};
+
+export const App: React.FC = () => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    if (canvasRef.current) startGame(canvasRef.current)
+  }, [canvasRef]);
+
+  return <canvas width="400" height="500" ref={canvasRef}></canvas>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
