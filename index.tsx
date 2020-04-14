876aa63da34f071967a679b1f9a8f9ad5518d308
Nick Nance <nance.nick@gmail.com>
2020-04-13 21:13:56 -0700

    initial game state

--- index.tsx
+++ index.tsx
@@ -1,29 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const startGame = (canvas: HTMLCanvasElement) => {
-  const wallSize = 12;
+type Board = {
+  width: number;
+  height: number;
+  wallSize: number;
+};
+
+type GameState = {
+  board: Board;
+};
+
+const initialState: GameState = {
+  board: {
+    width: 400,
+    height: 500,
+    wallSize: 12,
+  },
+};
+
+const drawBoard = (context: CanvasRenderingContext2D, { board }: GameState) => {
+  context.clearRect(0, 0, board.width, board.height);
+
+  // draw walls
+  context.fillStyle = "lightgrey";
+  context.fillRect(0, 0, board.width, board.wallSize);
+  context.fillRect(0, 0, board.wallSize, board.height);
+  context.fillRect(
+    board.width - board.wallSize,
+    0,
+    board.wallSize,
+    board.height
+  );
+};
+
+const startGame = (canvas: HTMLCanvasElement, state: GameState) => {
   const context = canvas.getContext("2d");
 
   if (context) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // draw walls
-    context.fillStyle = "lightgrey";
-    context.fillRect(0, 0, canvas.width, wallSize);
-    context.fillRect(0, 0, wallSize, canvas.height);
-    context.fillRect(canvas.width - wallSize, 0, wallSize, canvas.height);
+    drawBoard(context, state);
   }
 };
 
 export const App: React.FC = () => {
+  const { width, height } = initialState.board;
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   React.useEffect(() => {
-    if (canvasRef.current) startGame(canvasRef.current)
+    if (canvasRef.current) startGame(canvasRef.current, initialState);
   }, [canvasRef]);
 
-  return <canvas width="400" height="500" ref={canvasRef}></canvas>;
+  return <canvas width={width} height={height} ref={canvasRef}></canvas>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
